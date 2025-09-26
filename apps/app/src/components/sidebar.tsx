fddae2f6b1a5b2db35f3e6ea98b093fea100e0ed
rharkor <louis@huort.com>
2025-09-26 22:24:59 +0200

    fix: lint

--- apps/app/src/components/sidebar.tsx
+++ apps/app/src/components/sidebar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Home, List, LogOut, Server } from "lucide-react";
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "~/components/ui/button";
