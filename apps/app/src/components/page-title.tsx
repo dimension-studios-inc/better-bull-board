8e858f037a9c7bb6d5ce531b4c45bed9b20635cb
Julien Leveneur <jleveneur.pro@gmail.com>
2026-02-09 10:59:25 +0100

    feat: enhance back button functionality in PageTitle component
    - Implemented a new handleGoBack function to improve navigation logic.
    - Updated the onClick handler for the back button to use the new function, ensuring proper routing based on history length.

--- apps/app/src/components/page-title.tsx
+++ apps/app/src/components/page-title.tsx
@@ -15,11 +15,19 @@ export function PageTitle({
 }) {
   const router = useRouter();
 
+  const handleGoBack = () => {
+    if (window.history.length > 1) {
+      router.back();
+    } else {
+      router.push("/runs");
+    }
+  };
+
   return (
     <div>
       <div className="flex items-center gap-2">
         {withBackButton && (
-          <Button variant="ghost" size={"sm"} onClick={() => router.back()}>
+          <Button variant="ghost" size={"sm"} onClick={handleGoBack}>
             <ArrowLeft className="size-4" />
           </Button>
         )}
